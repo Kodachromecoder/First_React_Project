4c3d26aa89df5581b009e0511973c51f082978d0
Kara Kelly <ksquared9595@gmail.com>
2021-02-05 16:18:18 -0600

    React Router Parameters

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class CampsiteInfo extends Component {
 
-    renderCampsite(campsite) {
+
+function RenderCampsite({campsite}) {
         return (
             <div className="col-md-5 m-1">
                 <Card>
@@ -14,10 +14,10 @@ class CampsiteInfo extends Component {
                     </CardBody>
                 </Card>
           </div>
-        )
+        );
       }
 
-    renderComments(comments) {
+function RenderComments({comments}) {
         if(comments) {
             return (
                 <div className="col-md-5 m-1">
@@ -30,30 +30,29 @@ class CampsiteInfo extends Component {
                                     --{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
                                 </p>
                             </div>
-                        )
+                        );
                     })}
                 </div>
-            )
+            );
         }
-
+        return <div />;
     }
 
       
 
-    render() {
-        if (this.props.campsite) {
+function CampsiteInfo(props) {
+        if (props.campsite) {
             return (
                 <div className="container">
                     <div className="row">
-                        {this.renderCampsite(this.props.campsite)}
-                        {this.renderComments(this.props.campsite.comments)}
+                        <RenderCampsite campsite={props.campsite} />
+                        <RenderComments comments={props.comments} />
                     </div>
                 </div>
             );
         }
-        return <div />
+        return <div />;
     }
-}
 
 
 export default CampsiteInfo;
