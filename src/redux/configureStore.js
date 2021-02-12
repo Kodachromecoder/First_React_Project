e1f391dc0089f37a619711d86c2b5a4c41c81abb
Kara Kelly <ksquared9595@gmail.com>
2021-02-11 19:22:14 -0600

    Intro to Redux

---
+++ src/redux/configureStore.js
@@ -0,0 +1,12 @@
+import { Reducer, initialState } from './reducer';
+import { createStore } from 'redux';
+
+
+export const ConfigureStore = () => {
+    const store = createStore(
+        Reducer,
+        initialState
+    );
+
+    return store;
+};
