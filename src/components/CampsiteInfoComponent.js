7b03788b145c2fd7b62bf3da51a171d71db79129
Kara Kelly <ksquared9595@gmail.com>
2021-02-19 11:29:01 -0600

    Fetch from Server

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -5,6 +5,7 @@ import { Component } from 'react';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Input, Label, Col, Row  } from 'reactstrap';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const maxLength = len => val => !val || (val.length <= len);
 const minLength = len => val => val && (val.length >= len);
@@ -15,7 +16,7 @@ function RenderCampsite({campsite}) {
         return (
             <div className="col-md-5 m-1">
                 <Card>
-                    <CardImg top src={campsite.image} alt={campsite.name} />
+                    <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
                     <CardBody>
                         <CardText>{campsite.description}</CardText>
                     </CardBody>
